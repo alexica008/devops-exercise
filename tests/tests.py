ce87e4638f89f69c1651d018f327e1729ef63a23
alexica008 <alexica@protonmail.com>
2021-01-27 18:49:24 +0000

    trim test script

--- tests/tests.py
+++ tests/tests.py
@@ -3,9 +3,10 @@
 import requests
 
 page1 = requests.get('http://10.0.0.11')
-page2 = requests.get('http://10.0.0.11:')
+page2 = requests.get('http://10.0.0.11')
 
-if ("httpd_1" in page1.text or "httpd_2" in page1.text) and ("httpd_1" in page2.text or "httpd_2" in page2.text):
-  print ("da")
+if (("httpd_1" in page1.text or "httpd_2" in page1.text) and 
+   ("httpd_1" in page2.text or "httpd_2" in page2.text)):
+  print ("rr")
 else:
   print ("nu")
